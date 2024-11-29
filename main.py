dc49680e67bc4021b29996d4535d7a69a94c5ece
VadimRustam <vg1226205@gmail.com>
2024-11-29 11:27:46 +0500

    Update main.py add commit

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import tkinter as tk
-
+# Класс TicTacToe реализует интерфейс игры "Крестики-нолики"
 class TicTacToe:
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Крестики-нолики")
-        self.buttons = []
+        self.window = tk.Tk() # Инициализация окна приложения
+        self.window.title("Крестики-нолики") # Установлен заголовок окна
+        self.buttons = [] # Создан список для хранения кнопок игрового поля
 
-        for i in range(3):
-            for j in range(3):
-                button = tk.Button(
-                    self.window,
-                    text='',
-                    font=('Times New Roman', 25),
-                    width=10,
-                    height=5
+        # Создание игрового поля 3x3
+        for i in range(3): # Создание игрового поля 3x3
+            for j in range(3): # Создание игрового поля 3x3 
+                button = tk.Button( # Создание кнопки для игрового поля
+                    self.window, # Указан родительский элемент - главное окно
+                    text='',  # Установлен пустой текст на кнопке
+                    font=('Times New Roman', 25), # Указан шрифт текста
+                    width=10, # Установлена ширина кнопки
+                    height=5  # Установлена высота кнопки 
                 )
-                button.grid(row=i, column=j)
-                self.buttons.append(button)
+                button.grid(row=i, column=j) # Размещение кнопки в сетке 
+                self.buttons.append(button) # Добавление кнопки в список кнопок
+                
 
     def run(self):
-        self.window.mainloop()
+        self.window.mainloop() # Запуск обработки событий приложения 
 
 if __name__ == "__main__":
-    game = TicTacToe()
-    game.run()
+    game = TicTacToe() # Создание экземпляра игры TicTacToe
+    game.run()  # Запуск основного цикла приложения
