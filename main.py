a93a719b67ba89d59e27c0029b9793b8cc9a736a
VadimRustam <vg1226205@gmail.com>
2024-11-30 10:03:47 +0500

    Update, add MiniMax

--- main.py
+++ main.py
@@ -36,6 +36,77 @@ class TicTacToe:
             else:
                 self.current_player = 'O' if self.current_player == 'X' else 'X' меняет текущего игрока
                 
+     def button_click(self, row, col):
+        index = 3 * row + col
+        if self.board[index] == '':
+            self.board[index] = self.current_player
+            self.buttons[index].config(text=self.current_player, fg="navy")
+
+            if self.check_winner():
+                messagebox.showinfo("Победа!", f"Игрок {self.current_player} победил!")
+                self.reset_game()
+            elif '' not in self.board:
+                messagebox.showinfo("Ничья!", "Игра окончена вничью!")
+                self.reset_game()
+            else:
+                self.current_player = 'O'
+                self.ai_move()
+
+    def ai_move(self):
+        best_score = float('-inf')
+        best_move = None
+
+        for i in range(len(self.board)):
+            if self.board[i] == '':
+                self.board[i] = 'O'
+                score = self.minimax(self.board, 0, False)
+                self.board[i] = ''
+                if score > best_score:
+                    best_score = score
+                    best_move = i
+
+        if best_move is not None:
+            self.buttons[best_move].config(text='O', fg='red')
+            self.board[best_move] = 'O'
+
+            if self.check_winner():
+                messagebox.showinfo("Победа!", "Компьютер победил!")
+                self.reset_game()
+            elif '' not in self.board:
+                messagebox.showinfo("Ничья!", "Игра окончена вничью!")
+                self.reset_game()
+            else:
+                self.current_player = 'X'
+    def minimax(self, board, depth, is_maximizing):
+        if self.check_winner_board(board, 'O'):
+            return 1
+        if self.check_winner_board(board, 'X'):
+            return -1
+        if '' not in board:
+            return 0
+
+        if is_maximizing:
+            best_score = float('-inf')
+            for i in range(len(board)):
+                if board[i] == '':
+                    board[i] = 'O'
+                    score = self.minimax(board, depth + 1, False)
+                    board[i] = ''
+                    best_score = max(score, best_score)
+            return best_score
+        else:
+            best_score = float('inf')
+            for i in range(len(board)):
+                if board[i] == '':
+                    board[i] = 'X'
+                    score = self.minimax(board, depth + 1, True)
+                    board[i] = ''
+                    best_score = min(score, best_score)
+            return best_score
+            
+     def check_winner(self):
+        return self.check_winner_board(self.board, self.current_player)
+        
     def check_winner(self): # Метод проверки условий победы 
         for i in range(0, 9, 3):   # Проверка строк, проверяем каждые 3 подряд идущие ячейки 
             if self.board[i] == self.board[i + 1] == self.board[i + 2] != '': 
