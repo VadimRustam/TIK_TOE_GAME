d8981da89c81bed8ac50db4fb0e2b82deb50930b
VadimRustam <vg1226205@gmail.com>
2024-12-07 13:44:39 +0500

    Update main, add commit on Minimax fuction and button_click

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class TicTacToe:
         reset_button = tk.Button(
             self.window,
             text="Новая игра",
-            command=self.reset_game
+            command=self.reset_game # Размещение кнопки новая игра
         )
         reset_button.grid(row=3, column=1)
                 
@@ -42,79 +42,63 @@ class TicTacToe:
                 self.reset_game() # Сброс игры
             else:
                 self.current_player = "O"
-                sekf.ai_move()
-                
-     def button_click(self, row, col):
-        index = 3 * row + col
-        if self.board[index] == '':
-            self.board[index] = self.current_player
-            self.buttons[index].config(text=self.current_player, fg="navy")
-
-            if self.check_winner():
-                messagebox.showinfo("Победа!", f"Игрок {self.current_player} победил!")
-                self.reset_game()
-            elif '' not in self.board:
-                messagebox.showinfo("Ничья!", "Игра окончена вничью!")
-                self.reset_game()
-            else:
-                self.current_player = 'O'
                 self.ai_move()
 
-    def ai_move(self):
-        best_score = float('-inf')
-        best_move = None
+    def ai_move(self): # Метод для выполнения хода компьютером
+        best_score = float('-inf') # Метод для выполнения хода компьютером
+        best_move = None # Инициализация переменной для хранения лучшего хода
 
-        for i in range(len(self.board)):
-            if self.board[i] == '':
-                self.board[i] = 'O'
-                score = self.minimax(self.board, 0, False)
-                self.board[i] = ''
-                if score > best_score:
-                    best_score = score
-                    best_move = i
+        for i in range(len(self.board)): # Проходим по всем ячейкам игрового поля
+            if self.board[i] == '': # Если ячейка пуста, то идем дальше
+                self.board[i] = 'O' # Ставим O
+                score = self.minimax(self.board, 0, False) # Вычисляем оценку этого хода с помощью алгоритма Minimax
+                self.board[i] = ''  # Очищаем ячейку после оценки
+                if score > best_score: # Если текущая оценка хода лучше, чем предыдущая
+                    best_score = score # Обновляем лучший результат
+                    best_move = i # Обновляем лучший ход
 
-        if best_move is not None:
-            self.buttons[best_move].config(text='O', fg='red')
-            self.board[best_move] = 'O'
+        if best_move is not None: # Если был найден лучший ход
+            self.buttons[best_move].config(text='O', fg='red') # Отображаем "O" на соответствующей кнопке
+            self.board[best_move] = 'O' # Обновляем состояние игрового поля
 
-            if self.check_winner():
-                messagebox.showinfo("Победа!", "Компьютер победил!")
+            if self.check_winner():  # Проверка, есть ли победитель после хода компьютера
+                messagebox.showinfo("Победа!", "Компьютер победил!") # Если победа, выводим сообщение
                 self.reset_game()
             elif '' not in self.board:
-                messagebox.showinfo("Ничья!", "Игра окончена вничью!")
+                messagebox.showinfo("Ничья!", "Игра окончена вничью!") # Если поле полностью заполнено и нет пустых ячеек
                 self.reset_game()
             else:
-                self.current_player = 'X'
+                self.current_player = 'X' # Если игры продолжаются, ход переходит к игроку X
                 
     def minimax(self, board, depth, is_maximizing):
-        if self.check_winner_board(board, 'O'):
-            return 1
+        if self.check_winner_board(board, 'O'): # Проверка, выиграл ли игрок O компьютер
+            return 1 # Победа для компьютера, оценка 1
         if self.check_winner_board(board, 'X'):
-            return -1
-        if '' not in board:
-            return 0
+            return -1 # Проверка, выиграл ли игрок X
+        if '' not in board: # Если нет победителей и нет пустых ячеек ничья
+            return 0 # оценка 0
 
-        if is_maximizing:
-            best_score = float('-inf')
-            for i in range(len(board)):
-                if board[i] == '':
-                    board[i] = 'O'
-                    score = self.minimax(board, depth + 1, False)
-                    board[i] = ''
-                    best_score = max(score, best_score)
-            return best_score
+        if is_maximizing: # Ход компьютера
+            best_score = float('-inf') # Начинаем с минимально возможной оценки
+            for i in range(len(board)): # Проходим по всем ячейкам доски
+                if board[i] == '': # Если ячейка пуста
+                    board[i] = 'O' # Ставим "O" в эту ячейку
+                    score = self.minimax(board, depth + 1, False) # Рекурсивно вызываем minimax для следующего хода
+                    board[i] = '' # Очищаем ячейку после оценки
+                    best_score = max(score, best_score) # Обновляем лучший ход для компьютера
+            return best_score # Возвращаем лучший результат для хода компьютера
         else:
-            best_score = float('inf')
-            for i in range(len(board)):
-                if board[i] == '':
-                    board[i] = 'X'
-                    score = self.minimax(board, depth + 1, True)
-                    board[i] = ''
-                    best_score = min(score, best_score)
-            return best_score
+            best_score = float('inf') # Ход игрока
+            for i in range(len(board)): # Проходим по всем ячейкам доски
+                if board[i] == '': # Если ячейка пуста
+                    board[i] = 'X' # Ставим "X" в эту ячейку
+                    score = self.minimax(board, depth + 1, True) # Рекурсивно вызываем minimax для следующего хода
+                    board[i] = '' # Очищаем ячейку после оценки
+                    best_score = min(score, best_score) # Обновляем лучший ход для игрока
+            return best_score # Возвращаем лучший результат для хода игрока
             
-     def check_winner(self):
-        return self.check_winner_board(self.board, self.current_player)
+     def check_winner(self): 
+        return self.check_winner_board(self.board, self.current_player) # Проверка победителя для текущего игрока
         
     def check_winner(self): # Метод проверки условий победы 
         for i in range(0, 9, 3):   # Проверка строк, проверяем каждые 3 подряд идущие ячейки 
