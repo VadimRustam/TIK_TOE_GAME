7b2552448d0e67bc4a4aafc6b902acbc59a99c09
Vadim Gimazetdinov <vg1226205@gmail.com>
2025-06-26 12:13:43 +0500

    a

--- back-end/src/main/java/com/project_back_end/models/DoctorController.java
+++ back-end/src/main/java/com/project_back_end/models/DoctorController.java
@@ -1,8 +1,12 @@
 package com.project.back_end.controllers;
 
-import com.project.back_end.models.Doctor;
-import com.project.back_end.repositories.DoctorRepository;
+import com.project_back_end.models.Appointment;
+import com.project_back_end.models.Doctor;
+import com.project_back_end.repositories.DoctorRepository;
+import com.project_back_end.services.DoctorService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +18,9 @@ public class DoctorController {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    @Autowired
+    private DoctorService doctorService;
+
     @GetMapping
     public List<Doctor> getAllDoctors() {
         return doctorRepository.findAll();
@@ -22,5 +29,18 @@ public class DoctorController {
     @PostMapping
     public Doctor createDoctor(@RequestBody Doctor doctor) {
         return doctorRepository.save(doctor);
+    }
+
+    // ✅ Новый метод для получения доступности врача
+    @GetMapping("/availability")
+    public ResponseEntity<?> getDoctorAvailability(
+            @RequestParam Long doctorId,
+            @RequestParam String date,
+            @RequestHeader("Authorization") String token) {
+
+        // TODO: Валидация токена и извлечение роли пользователя
+
+        List<Appointment> appointments = doctorService.getAvailabilityByDoctorAndDate(doctorId, date);
+        return ResponseEntity.ok(appointments);
     }
 }
